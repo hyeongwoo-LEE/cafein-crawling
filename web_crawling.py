087b8571391bacdffc77e74a784941ce137d1ee4
hyeongwoo-LEE <lhw970804@naver.com>
2022-08-30 03:29:18 +0900

    Update web_crawling.py

--- web_crawling.py
+++ web_crawling.py
@@ -5,7 +5,6 @@ import requests
 from bs4 import BeautifulSoup
 import time
 
-# usb 가져오지 못한다는 에러 자꾸 떠서 추가함
 options = webdriver.ChromeOptions()
 options.add_experimental_option("excludeSwitches", ["enable-logging"])
 browser = webdriver.Chrome(options=options)
