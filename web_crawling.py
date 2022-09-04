9a89aa3af579d259a1c3af36b0b736ba615806fe
hyeongwoo-LEE <77663506+hyeongwoo-LEE@users.noreply.github.com>
2022-09-04 19:26:40 +0900

    feat : 카페명 출력 완료

--- web_crawling.py
+++ web_crawling.py
@@ -32,7 +32,7 @@ def time_wait(num, code):
 query = '투썸 명동사거리점'
 
 browser.get("https://m.search.naver.com/search.naver?query="+query)
-time.sleep(1)
+time.sleep(0.5)
 
 browser.find_element(By.XPATH, '//*[@id="place-main-section-root"]/div/div[3]/div/ul/li[2]/div/a/div/div').click()
 a = browser.find_element(By.XPATH, '//*[@id="_title"]/a/span[1]').text
@@ -41,10 +41,11 @@ soup = BeautifulSoup(browser.page_source, 'html.parser')
 
 span_tags = soup.find_all("span", {'class':'ob_be'})
 
+print(browser.find_element(By.XPATH, '//*[@id="_title"]/a/span[1]').text)
 for span_tag in span_tags:
+    
     day_text = span_tag.find("span", {'class':'kGc0c'})
     time_text = span_tag.find("div", {'class': 'qo7A2'})
-    
     print(day_text.get_text(),": ", time_text.get_text())
     
     
