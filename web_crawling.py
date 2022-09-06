9a7d84029668cb5de3cfb7763f0797a1def86050
hyeongwoo-LEE <77663506+hyeongwoo-LEE@users.noreply.github.com>
2022-09-07 03:45:20 +0900

    feat : 영업시간 데이터 수집 - 예외 케이스 처리

--- web_crawling.py
+++ web_crawling.py
@@ -21,18 +21,20 @@ def toJson(store_list):
 
 
 
-#검색 keyword로 보낼 json 파일 로딩
+# 검색 keyword로 보낼 json 파일 로딩
 with open('store_list_동대문구.json','r', encoding='utf-8-sig') as f:
     data = json.load(f)
     total_cnt = data['totalCnt']
     items = data['storeList']
     
     
-#카페 빈 리스트 생성
+# json 빈 리스트 생성
 store_list = []
 
+not_exist = []
+
 for item in items:
-    query = item['storeName']
+    query = item['storeName'].replace("메가커피","메가MGC커피")
     browser.get("https://m.place.naver.com/place/list?query="+ query + "&level=top")
 
     time.sleep(3)
@@ -40,7 +42,7 @@ for item in items:
     soup = BeautifulSoup(browser.page_source, 'html.parser')
 
 
-    #검색 결과 다중
+    # 검색 다중 결과
     store_list = soup.find_all('span', {'class':'place_bluelink YwYLL'})
 
     loop_cnt = 1;
@@ -57,6 +59,7 @@ for item in items:
         loop_cnt += 1
     else:
         print("검색 카페가 존재하지 않음 : ", query)
+        not_exist.append(query)
         continue
     
     time.sleep(6)
@@ -69,54 +72,82 @@ for item in items:
          
     soup = BeautifulSoup(browser.page_source, 'html.parser')
 
-    #카페별 딕셔너리 생성
+    # 카페별 딕셔너리 생성
     store_dict = {"storeName" : query}
     
-    #전화번호 데이터
+    # 전화번호 데이터
     try:
         phone = soup.find('span', {'class':'dry01'}).get_text()
         store_dict['phone'] = phone
     except:
         store_dict['phone'] = None
 
-    #영업시간 데이터
+    # 영업시간 데이터
     span_tags = soup.find_all('span', {'class':'ob_be'})
 
-    #영업시간 딕셔너리 생성
+    # 영업시간 딕셔너리 생성
     store_dict['businessHours'] = {'onMon':None, 'onTue':None, 'onWed':None, 'onThu':None, 'onFri':None, 'onSat':None, 'onSun':None}
-
-    cnt = 0
+   
+    # 요일별 영업시간 데이터 수집
     for span_tag in span_tags:
         
-        if(span_tag.find("span", {'class':'kGc0c'}) == None): break
+        if(span_tag.find("span", {'class':'kGc0c'}) == None): 
+            continue
         
+        # 요일 데이터
         day_text = span_tag.find("span", {'class':'kGc0c'}).get_text()
-        day_text = day_text[0]
+        
+        if(day_text != '매일'):
+            day_text = day_text[0]
+        
+        # 영업시간 데이터
         time_text = span_tag.find("div", {'class': 'qo7A2'}).get_text()
+
+        if "새벽" in time_text: continue
         
         split_time = time_text.replace(" ","").split("-")
         
-        if(len(split_time) < 2):continue
+        if(len(split_time) < 2): continue
+         
+        open = split_time[0][0:5]
+        closed = split_time[1][0:5]
+
+        business_hour_dict = {'open':open, 'closed':closed}
         
         if(day_text == '월'): 
-            store_dict['businessHours']['onMon'] = {'open':split_time[0], 'closed':split_time[1]}
+            store_dict['businessHours']['onMon'] = business_hour_dict
         elif(day_text == '화'): 
-            store_dict['businessHours']['onTue'] = {'open':split_time[0], 'closed':split_time[1]}
+            store_dict['businessHours']['onTue'] = business_hour_dict
         elif(day_text == '수'): 
-            store_dict['businessHours']['onWed'] = {'open':split_time[0], 'closed':split_time[1]}
+            store_dict['businessHours']['onWed'] = business_hour_dict
         elif(day_text == '목'): 
-            store_dict['businessHours']['onThu'] = {'open':split_time[0], 'closed':split_time[1]}
+            store_dict['businessHours']['onThu'] = business_hour_dict
         elif(day_text == '금'): 
-            store_dict['businessHours']['onFri'] = {'open':split_time[0], 'closed':split_time[1]}
+            store_dict['businessHours']['onFri'] = business_hour_dict
         elif(day_text == '토'): 
-            store_dict['businessHours']['onSat'] = {'open':split_time[0], 'closed':split_time[1]}
+            store_dict['businessHours']['onSat'] = business_hour_dict
         elif(day_text == '일'): 
-            store_dict['businessHours']['onSun'] = {'open':split_time[0], 'closed':split_time[1]}
+            store_dict['businessHours']['onSun'] = business_hour_dict
+        elif(day_text == '매일'):
+            store_dict['businessHours']['onMon'] = business_hour_dict
+            store_dict['businessHours']['onTue'] = business_hour_dict
+            store_dict['businessHours']['onWed'] = business_hour_dict
+            store_dict['businessHours']['onThu'] = business_hour_dict
+            store_dict['businessHours']['onFri'] = business_hour_dict
+            store_dict['businessHours']['onSat'] = business_hour_dict
+            store_dict['businessHours']['onSun'] = business_hour_dict
+            break
+            
             
         
     store_list.append(store_dict)
     print(store_list)
 
+# 검색 결과가 없는 카페 출력
+for store_name in not_exist:
+    print(store_name)
+    
+#수집한 데이터로 json 파일 생성
 toJson(store_list)
     
     
